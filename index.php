cec255647c1d1b3943819cb9dc8ab34a6772ebfc
Martin Vician <martin.vician@nic.cz>
2016-08-24 15:49:57 +0200

    added source, google tracking

--- index.php
+++ index.php
@@ -205,6 +205,8 @@ echo "</div>";
 		- Lucie Medová<br/>
 		- Klára Viciánová<br/>
 		- <a href="https://www.vician.cz/">Martin Vicián</a> (web, technologie)
+		<hr/>
+		Zdroj dat: <a href="http://www.mvcr.cz/clanek/cetnost-jmen-a-prijmeni.aspx">MVČR</a>
             </div>
           </div>
         </div>
@@ -236,5 +238,17 @@ echo "</div>";
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 		<script src='https://www.google.com/recaptcha/api.js'></script>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-73656343-3', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
 	</body>
 </html>
