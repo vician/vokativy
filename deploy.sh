58a9b381fcabbc3064fbaa6ad3a5cb983a883d1e
Martin Vician <martin.vician@nic.cz>
2016-08-30 10:38:15 +0200

    deploy to test

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-scp current.sqlite3 vf.vician.cz:/var/www/vokativy.cz/
+dir=""
+
+if [ $# -eq 1 ]; then
+	if [ $1 == "test" ]; then
+		dir="test/"
+	fi
+fi
+
+scp current.sqlite3 vf.vician.cz:/var/www/vokativy.cz/$dir
